941758d0c21b90f25b378cbe43bbff9fac7cedd5
Adyanth H <adyanthh@gmail.com>
2021-07-15 19:05:39 +0530

    :rotating_light: Fix lint warnings

--- app.go
+++ app.go
@@ -11,6 +11,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// Run migrates the DB and starts the web server
 func (app *App) Run() {
 	app.db.AutoMigrate(&Link{})
 	app.db.AutoMigrate(&File{})
@@ -19,6 +20,7 @@ func (app *App) Run() {
 	app.handleRequests()
 }
 
+// NewApp creates a new App instance with the provided bind address and storage path
 func NewApp(bind, storagePath string) App {
 	usr, _ := user.Current()
 	if storagePath == "~" {
@@ -29,13 +31,13 @@ func NewApp(bind, storagePath string) App {
 	os.MkdirAll(storagePath, 0700)
 
 	dbUri := path.Join(storagePath, "mapping.db")
-	if db, err := gorm.Open(sqlite.Open(dbUri), &gorm.Config{}); err == nil {
+	if db, err := gorm.Open(sqlite.Open(dbUri), &gorm.Config{}); err != nil {
+		panic(fmt.Errorf("db error %v", err))
+	} else {
 		return App{
 			bind:        bind,
 			db:          db,
 			storagePath: storagePath,
 		}
-	} else {
-		panic(fmt.Errorf("db error %v", err))
 	}
 }
--- types.go
+++ types.go
@@ -4,18 +4,21 @@ import (
 	"gorm.io/gorm"
 )
 
+// App struct containing the bind address, storage path and the db connector.
 type App struct {
 	bind        string
 	db          *gorm.DB
 	storagePath string
 }
 
+// Link struct for saving the Redirect Links /l/.
 type Link struct {
 	ID   string `gorm:"primaryKey" json:"id" validate:"required,min=3,max=32,alphanumunicode"`
 	Link string `json:"link" validate:"required,url"`
 	gorm.Model
 }
 
+// File struct for saving the file uploads /f/.
 type File struct {
 	ID   string `gorm:"primaryKey" json:"id" validate:"required,min=3,max=32,alphanumunicode"`
 	Name string `json:"name"`
@@ -23,6 +26,7 @@ type File struct {
 	gorm.Model
 }
 
+// Text struct for saving the text pastes /t/.
 type Text struct {
 	ID   string `gorm:"primaryKey" json:"id" validate:"required,min=3,max=32,alphanumunicode"`
 	Type string `validate:"omitempty,oneof=txt md"`
