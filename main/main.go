2f728cf21174047bcbbab1667e8b7095efaa7017
Adyanth H <adyanthh@gmail.com>
2021-07-15 16:15:27 +0530

    :truck: Move main to subfolder

---
+++ main/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"git.adyanth.site/adyanth/shortpaste"
+)
+
+func main() {
+	bind, ok := os.LookupEnv("BIND_ADDR")
+	if !ok {
+		bind = ":8080"
+	}
+
+	storagePath, ok := os.LookupEnv("STORAGE_PATH")
+	if !ok {
+		storagePath = "~/.shortpaste"
+	}
+
+	app := shortpaste.NewApp(bind, storagePath)
+	app.Run()
+}
