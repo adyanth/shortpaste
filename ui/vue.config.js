c350638bd108d6b2d3995c8f2d678f704c7bb12c
Adyanth H <adyanthh@gmail.com>
2021-07-18 19:20:04 +0530

    :truck: Show correct title in UI

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Short{Paste}'
+        return args
+      })
+  },
   transpileDependencies: [
     'vuetify'
   ],
